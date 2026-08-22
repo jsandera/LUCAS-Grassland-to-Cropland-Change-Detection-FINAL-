c9f4b851c3b1904085f4d2370ab12fa7de5942e4
jsandera <89800209+jsandera@users.noreply.github.com>
2026-08-22 11:52:55 +0200

    Add files via upload

---
+++ styles/LUCASPoints_3_style.js
@@ -0,0 +1,34 @@
+var size = 0;
+var placement = 'point';
+
+var style_LUCASPoints_3 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "16.900000000000002px \'Open Sans\', sans-serif";
+    var labelFill = "#e31a1c";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if (feature.get("point_id") !== null) {
+        labelText = String(feature.get("point_id"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(227,26,28,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+
+    return style;
+};
